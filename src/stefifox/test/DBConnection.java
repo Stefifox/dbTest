96797dcc6cdf01a7d7cea849e7256367163fe2f3
Stefano <30950154+Stefifox@users.noreply.github.com>
2020-04-11 20:05:02 +0200

    Add source code

---
+++ src/stefifox/test/DBConnection.java
@@ -0,0 +1,90 @@
+package stefifox.test;
+
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class DBConnection {
+
+	public static int getLastId() throws SQLException {
+		
+		int id = 0; 
+		
+		Connection connection = DriverManager.getConnection(Main.Connection, Main.User, Main.Pass );
+		Statement stm = connection.createStatement();
+		ResultSet rs=stm.executeQuery("SELECT id FROM " + Main.tableName);
+		
+		while(rs.next()) {
+			id=rs.getInt(1);
+		}
+		
+		stm.close();
+		
+		return id;
+		
+		
+		
+	}
+	
+	public static void addRow(String event_name, String description) throws SQLException {
+		
+
+		Connection connection = DriverManager.getConnection(Main.Connection, Main.User, Main.Pass );
+		PreparedStatement prepared = connection.prepareStatement("INSERT INTO " + Main.tableName + " (id, date, event_name, description) VALUES (?,?,?,?)");
+		Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"),Locale.ITALY);
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		
+		String data = simpleDateFormat.format(calendar.getTime());
+		
+		prepared.setInt(1, getLastId() + 1);
+		prepared.setString(2, data);
+		prepared.setString(3, event_name);
+		prepared.setString(4, description);
+		prepared.executeUpdate();
+		
+		
+	}
+	
+	public static String getLastLineById() throws SQLException {
+		
+		Connection connection = DriverManager.getConnection(Main.Connection, Main.User, Main.Pass );
+		Statement stm = connection.createStatement();
+		ResultSet rs=stm.executeQuery("SELECT * FROM " + Main.tableName + " WHERE id = " + getLastId());
+		
+		rs.next();
+		return rs.getInt(1) + " - " + rs.getString(2) + " - " + rs.getString(3) + " - " + rs.getString(4);
+		
+		
+	}
+	
+	public static String getLineById(int id) throws SQLException {
+		
+		Connection connection = DriverManager.getConnection(Main.Connection, Main.User, Main.Pass );
+		Statement stm = connection.createStatement();
+		ResultSet rs=stm.executeQuery("SELECT * FROM " + Main.tableName + " WHERE id = " + id);
+		
+		rs.next();
+		return rs.getInt(1) + " - " + rs.getString(2) + " - " + rs.getString(3) + " - " + rs.getString(4);
+		
+		
+	}
+	
+	
+	public static HashMap<Integer, String> getAll() throws SQLException{
+		
+		Connection connection = DriverManager.getConnection(Main.Connection, Main.User, Main.Pass );
+		Statement stm = connection.createStatement();
+		ResultSet rs=stm.executeQuery("SELECT * FROM " + Main.tableName);
+		
+		HashMap<Integer, String> temp = new HashMap<>();
+		
+		while(rs.next()) {
+			temp.put(rs.getInt(1), rs.getString(2) + " - " + rs.getString(3) + " - " + rs.getString(4));
+		}
+		stm.close();
+		return temp;
+		
+		
+	}
+	
+}
---
+++ src/stefifox/test/Main.java
@@ -0,0 +1,41 @@
+package stefifox.test;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class Main {
+
+public static String Connection = "jdbc:mysql://localhost:3306/provaDB?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"; //Replace "provaDB" with your DB name and UTC with your Timezone
+public static String tableName = "event"; // Replace with your table name
+
+public static String User = "root"; // Replace with your DB User
+public static String Pass = "root"; // Replace with your DB user password 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashMap<Integer, String> tabella = null;
+		
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}
+		catch (ClassNotFoundException e){
+		    // gestione dell'eccezione
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println(DBConnection.getLastLineById());
+			DBConnection.addRow("evento2", "Sono una descrizione");
+			System.out.println(DBConnection.getLineById(DBConnection.getLastId()));
+			tabella = DBConnection.getAll();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
